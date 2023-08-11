6f58e9ffd47aca9f20a13a24e4dfbba93ac73161
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-08-10 23:39:25 -0300

    agrego correcciones y anotaciones

--- calculadora-simple/script.js
+++ calculadora-simple/script.js
@@ -1,31 +1,43 @@
 
 function calcular() {
-    let resultado;
-    let numero1 = document.getElementById("número1").value;
-    let numero2 = document.getElementById("número2").value;
-    let operacion = document.getElementById("operación").value;
+    //primero traemos las variables desde el html...
+    var res = document.getElementById("resultado").value;
+    var num1 = document.getElementById("número1").value;
+    var num2 = document.getElementById("número2").value;
+    var oper = document.getElementById("operación").value;
 
-    if (operacion==="none"||numero1===""||numero2==="") {
-        alert("Complete todos los campos");
+    if (oper === "" || num1 === "" || num2 === "") {
+        res = "error,faltan datos";
+        //el primer if valida que los campos no esten vacíos
     } else {
-        numero1 = Number(numero1);
-        numero2 = Number(numero2);
-        if(operacion==="sumar") {
-            resultado=numero1+numero2;
-        } else if(operacion==="restar") {
-            resultado=numero1-numero2;
-        } else if(operacion==="multiplicar") {
-            resultado=numero1*numero2;
-        } else if(operacion==="dividir") {
-            if(numero2===0){
-                alert("Ingrese un divisor que no sea 0");
-            } else {
-                resultado=numero1/numero2;
-            }
-        } if((resultado.toString()).length > 20) {
-            alert("El resultado tiene demasiados carácteres");
-        } else {
-            document.getElementById("resultado").innerHTML = (resultado);
-        }
+        //pasamos los value num1 y 2 a números
+        num1 = Number(num1);
+        num2 = Number(num2);
+        switch(oper) {
+            //en principio probé if anidados pero al final usé switch case para las operaciones
+            case "sumar":
+            res = num1 + num2;
+            break;
+            case "restar":
+            res = num1-num2;
+            break;
+            case "multiplicar":
+            res = num1 * num2;
+            break;
+            case "dividir":
+                if(num2 === 0){
+                    //este if valida que no se divida x 0
+                    res = "error,no dividir x 0";
+                } else {
+                    res = num1 / num2;
+                }
+        } 
+    } if((res.toString()).length > 20) {
+        //este if da el limite de 20 digitos al resultado
+        alert("Resultado demasiado extenso, máximo 20 dígitos");
+    } else {
+        document.getElementById("resultado").innerHTML = (res);
     }
 }
+//evento click para el botón
+document.getElementById("botón").addEventListener("click", calcular);
