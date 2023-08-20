586df0e8efc873e11708e97729b7da1890488404
Edgardo Ezequiel Sarmiento <129633447+EzeSar@users.noreply.github.com>
2023-08-20 01:41:32 -0300

    parte 4

--- piedra-papel-tijera/README.md
+++ piedra-papel-tijera/README.md
@@ -24,35 +24,73 @@ Acá van las instrucciones
 
 ### PARTE 2: Obtención del nombre del jugador
 
-* `script.js` archivos para las acciones dinamicas de la app:
+* `index.html`: botón para ingresar el nombre del usuario.
+
+```html
+<div class="nombre">
+        <label>Tu nombre: <input type="text" id="nombre"></label>
+        <button id="enviar" onclick="agregarNombre()">Enviar</button>
+    </div>
+```
+
+* `script.js`: archivos para las acciones dinamicas de la app.
     - `agregarNombre()`: funcion para tomar el nombre del usuario. Contiene validación de input no vacío. Esta vinculada a un evento de tipo *onclick* en el *input* con `id="nombre"`.
+
+```javascript
+/* Funcion que toma el nombre */
+function agregarNombre(){
+    /* if que obliga a ingresar el nombre */
+    if(document.getElementById("nombre").value === ""){ 
+        alert("INGRESA TU NOMBRE!");
+    } else {
+        nombreUsuario = document.getElementById("nombre").value;
+    }
+}
+```
 
 ### PARTE 3: Selección de la opción del jugador y del oponente (PC)
 
-* En el `index.html` a cada botón (piedra, papel y tijera) se le agrega su correspondiente imágen `<img src=` y evento *onclick* `<button onclick=`.
-* En el `script.js` se incorporan funciones para capturar la opción seleccionada por el jugador y generar aleatoriamente la opción del oponente (PC).
+* `index.html`: a cada botón (piedra, papel y tijera) se le agrega su correspondiente imágen `<img src=` y evento *onclick* `<button onclick=`.
+
+* `script.js`: se incorporan funciones para capturar la opción seleccionada por el jugador y generar aleatoriamente la opción del oponente (PC).
+
  ```javascript
  /* función que genera aleatoriamente la jugada de la compu */
-function obtenerJugadaComputadora(){
+function obtenerJugadaCompu(){
     let jugadas = ["piedra","papel","tijeras"];
-    jugadaComputadora = jugadas[Math.floor(Math.random() * 2.9)];
+    jugadaCompu = jugadas[Math.floor(Math.random() * 2.9)];
 }
 
 /* las 3 funciones para las jugadas del usuario */
 function elijePiedra(){
     jugadaUsuario = "piedra";
-    alert(jugadaUsuario);
 }
 
 function elijePapel(){
     jugadaUsuario = "papel";
-    alert(jugadaUsuario);
 }
 
 function elijeTijera(){
     jugadaUsuario = "tijera";
-    alert(jugadaUsuario);
 }
  ```
+
+### PARTE 4: Determinación del ganador y actualización del marcador
+
+* Por definición general la opción "piedra" le gana a "tijera", "papel" le gana a "piedra", y "tijera" le gana a "papel".
+
+* `script.js`: se agregan funciones para comparar las opciones del jugador y del oponente y determinar el ganador de la ronda.
+
+* `index.html`: se agregan los marcadores de resultados.
+
+* `script.js`: se agrega función que actualiza los marcadores luego de cada ronda.
+
+```javascript
+function jugada(){
+    obtenerJugadaCompu();
+    determinarGanador(jugadaUsuario,jugadaCompu);
+    actualizarMarcadores();
+}
+```
 
 ---
--- piedra-papel-tijera/script.js
+++ piedra-papel-tijera/script.js
@@ -1,37 +1,106 @@
 /* inicialización de las variables a utilizar */
 var nombreUsuario;
 var jugadaUsuario;
-var jugadaComputadora;
+var jugadaCompu;
+var resultadoJugada;
+var ganadosUsuario = 0;
+var ganadosCompu = 0;
+var resultadoFinal;
 
 /* Funcion que toma el nombre */
 function agregarNombre(){
+    /* if que obliga a ingresar el nombre */
     if(document.getElementById("nombre").value === ""){
-        alert("INGRESE UN NOMBRE!");
+        alert("INGRESA TU NOMBRE!");
     } else {
         nombreUsuario = document.getElementById("nombre").value;
-        alert(nombreUsuario);/* solo para comprobar funcionamiento */
     }
 }
 
 /* función que genera aleatoriamente la jugada de la compu */
-function obtenerJugadaComputadora(){
-    let jugadas = ["piedra","papel","tijeras"];
-    jugadaComputadora = jugadas[Math.floor(Math.random() * 2.9)];
+function obtenerJugadaCompu(){
+    let jugadas = ["piedra","papel","tijera"];
+    jugadaCompu = jugadas[Math.floor(Math.random() * 2.9)];
 }
 
 /* las 3 funciones para las jugadas del usuario */
 function elijePiedra(){
-    jugadaUsuario = "piedra";
-    alert(jugadaUsuario);/* solo para comprobar funcionamiento */
+    if(document.getElementById("nombre").value === ""){
+        alert("INGRESA TU NOMBRE!");
+    } else {
+        jugadaUsuario = "piedra";
+        jugada();
+    }
 }
 
 function elijePapel(){
-    jugadaUsuario = "papel";
-    alert(jugadaUsuario);/* solo para comprobar funcionamiento */
+    if(document.getElementById("nombre").value === ""){
+        alert("INGRESA TU NOMBRE!");
+    } else {
+        jugadaUsuario = "papel";
+        jugada();
+    }
 }
 
 function elijeTijera(){
-    jugadaUsuario = "tijera";
-    alert(jugadaUsuario);/* solo para comprobar funcionamiento */
+    if(document.getElementById("nombre").value === ""){
+        alert("INGRESA TU NOMBRE!");
+    } else {
+        jugadaUsuario = "tijera";
+        jugada();
+    }
+}
+
+/* función que compara las jugadas y determina el ganador */
+function determinarGanador(a,b){
+    switch (a+b) {
+        case "piedrapapel":
+            resultadoJugada = `gana la computadora`;
+            ganadosCompu++;
+            break;
+        
+        case "piedratijera":
+            resultadoJugada = `gana ${nombreUsuario}`;
+            ganadosUsuario++;
+            break;
+        
+        case "papelpiedra":
+            resultadoJugada = `gana ${nombreUsuario}`;
+            ganadosUsuario++;
+            break;
+
+        case "papeltijera":
+            resultadoJugada = `gana la computadora`;
+            ganadosCompu++;
+            break;
+
+        case "tijerapiedra":
+            resultadoJugada = `gana la computadora`;
+            ganadosCompu++;
+            break;
+
+        case "tijerapapel":
+            resultadoJugada = `gana ${nombreUsuario}`;
+            ganadosUsuario++;
+            break;
+
+        default:
+            resultadoJugada = "empate, vuelve a jugar";
+        }
+}
+
+/* función que ejecuta la jugada */
+function jugada(){
+    obtenerJugadaCompu();
+    determinarGanador(jugadaUsuario,jugadaCompu);
+    actualizarMarcadores();
+}
+
+/* función que va actualizando los marcadores luego de cada jugada */
+function actualizarMarcadores(){
+    document.getElementById("resultado-jugada").innerHTML = `Resultado de la ronda: ${nombreUsuario} eligió ${jugadaUsuario}, la computadora eligió ${jugadaCompu}, ${resultadoJugada}`;
+    document.getElementById("ganados-usuario").innerHTML = `${nombreUsuario} : ${ganadosUsuario}`;
+    document.getElementById("ganados-compu").innerHTML = `Computadora : ${ganadosCompu}`;
+
 }
 
